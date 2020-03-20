3497833b5ea753fbfedd4e54808224c0ceaaebe2
shiwei93 <stayfocusjs@gmail.com>
2020-03-20 21:01:19 +0800

    redo: question 2

--- LeetCode.playground/Sources/Medium/2.AddTwoNumbers.swift
+++ LeetCode.playground/Sources/Medium/2.AddTwoNumbers.swift
@@ -2,21 +2,21 @@ import Foundation
 
 /// https://leetcode-cn.com/problems/add-two-numbers/
 public func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-    let result = ListNode(0)
+    let result: ListNode? = ListNode(0)
     var l1 = l1, l2 = l2, current = result
-    var index = 0
+    var index = 0; // 进位
     while l1 != nil || l2 != nil {
         let left = l1?.val ?? 0
         let right = l2?.val ?? 0
-        let total = left + right + index
+        let r = left + right + index
+        index = r / 10
+        current?.next = ListNode(r % 10)
+        current = current?.next
         l1 = l1?.next
         l2 = l2?.next
-        index = total / 10
-        current.next = ListNode(total % 10)
-        current = current.next!
     }
     if index == 1 {
-        current.next = ListNode(1)
+        current?.next = ListNode(1)
     }
-    return result.next
+    return result?.next
 }
