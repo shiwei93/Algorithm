ca7a4b1cda4c4cdb955cfec14efd8a4b6d07ffe4
shiwei93 <stayfocusjs@gmail.com>
2020-03-24 16:52:34 +0800

    complete leetcode the-masseuse-lcci

--- LeetCode.playground/Sources/Easy/Interview17.16.TheMesseuseLCCI.swift
+++ LeetCode.playground/Sources/Easy/Interview17.16.TheMesseuseLCCI.swift
@@ -2,13 +2,20 @@ import Foundation
 
 /// https://leetcode-cn.com/problems/the-masseuse-lcci/
 public func massage(_ nums: [Int]) -> Int {
-    if nums.isEmpty { return 0 }
-    if nums.count == 1 { return nums[0] }
-    var dp: [Int] = Array(repeating: 0, count: nums.count)
-    dp[0] = nums[0]
-    dp[1] = max(dp[0], nums[1])
-    for i in stride(from: 2, to: nums.count, by: 1) {
-        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+//    if nums.isEmpty { return 0 }
+//    if nums.count == 1 { return nums[0] }
+//    var dp: [Int] = Array(repeating: 0, count: nums.count)
+//    dp[0] = nums[0]
+//    dp[1] = max(dp[0], nums[1])
+//    for i in stride(from: 2, to: nums.count, by: 1) {
+//        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+//    }
+//    return dp[nums.count - 1]
+    var a = 0, b = 0
+    for i in stride(from: 0, to: nums.count, by: 1) {
+        let c = max(b, a + nums[i])
+        a = b
+        b = c
     }
-    return dp[nums.count - 1]
+    return b
 }
